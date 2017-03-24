a70b724ca7c91091976839848a25407780d05a64
Lee-Roy Ashworth <Lee-Roy.Ashworth@derivco.co.za>
2017-03-24 21:32:42 +0200

    chore: moved tests into repo

---
+++ tzatziki.minutz.core.tests/tzatziki.minutz.core.tests/TokenStringHelperTest.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tzatziki.minutz.interfaces;
+
+namespace tzatziki.minutz.core.tests
+{
+	[TestClass]
+	public class TokenStringHelperTest
+	{
+		private readonly ITokenStringHelper _tokenStringHelper;
+		private const string dateTestString =  "\"2017-03-18T08:41:51.9Z\"";
+		private DateTime dateComparison = DateTime.Parse("2017-03-18T08:41:51.9Z");
+		public TokenStringHelperTest()
+		{
+			_tokenStringHelper = new TokenStringHelper();
+		}
+
+		[TestMethod]
+		public void ConvertTokenStringToDate_ShouldReturnDateTimeType()
+		{
+			var result = _tokenStringHelper.ConvertTokenStringToDate("someDate");
+			Assert.IsInstanceOfType(result, typeof(DateTime));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ConvertTokenStringToDate_ShouldThrowErrorExceptionIfEmptyStringSupplied() 
+		{
+			var result = _tokenStringHelper.ConvertTokenStringToDate(string.Empty);
+		}
+
+		[TestMethod]
+		public void ConvertTokenStringToDate_ShouldReturnActualDate() 
+		{
+			var result = _tokenStringHelper.ConvertTokenStringToDate(dateTestString);
+			Assert.AreEqual(result, dateComparison);
+		}
+	}
+}
