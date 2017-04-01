376c4c537de7b9cb96b3e0b175da6006d67dff05
Lee-Roy Ashworth <Lee-Roy.Ashworth@derivco.co.za>
2017-04-01 23:12:20 +0200

    chore: moving auth0 projects

---
+++ tzatziki.minutz.auth0.service/Repository.cs
@@ -0,0 +1,15 @@
+﻿using tzatziki.minutz.interfaces.Repositories;
+using Microsoft.AspNetCore.Authentication;
+
+namespace tzatziki.minutz.auth0.service
+{
+  public class Repository : IAuth0Repository
+  {
+    public string Getrole(TicketReceivedContext context)
+    {
+      return "user";
+    }
+
+  }
+
+}
