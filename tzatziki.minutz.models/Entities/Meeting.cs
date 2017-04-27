edfa505feea5c2f9ef7518f5aa931eca6144bc28
Lee-Roy Ashworth <leeroya@gmail.com>
2017-04-27 14:53:15 +0200

    feat: added meeting models

---
+++ tzatziki.minutz.models/Entities/Meeting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tzatziki.minutz.models.Entities
+{
+  public class Meeting
+  {
+    /// <summary>
+    /// IsLocked this is tell the UI if the meeting is readonly for running the meeting
+    /// </summary>
+    public bool IsLocked { get; set; }
+
+    public string Location { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+    public int Duration { get; set; }
+    public bool IsReacurance { get; set; }
+    public bool IsPrivate { get; set; }
+    public bool IsFormal { get; set; }
+    public string TimeZone { get; set; }
+    public string[] Tag { get; set; }
+    public List<MeetingAttendee> MeetingAttendeeCollection { get; set; }
+    public string Purpose { get; set; }
+    public string Outcome { get; set; }
+    public List<MeetingAgendaItem> MeetingAgendaCollection { get; set; }
+    public List<ActionItem> MeetingActionCollection { get; set; }
+    public List<MeetingNoteItem> MeetingNoteCollection { get; set; }
+    public List<MeetingAttachmentItem> MeetingAttachmentCollection { get; set; }
+  }
+}
---
+++ tzatziki.minutz.models/Entities/MeetingNoteItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tzatziki.minutz.models.Entities
+{
+  public class MeetingNoteItem
+  {
+    public int Id { get; set; }
+    public int ReferanceId { get; set; }
+    public string NoteText { get; set; }
+    public int MeetingAttendeeId { get; set; }
+    public DateTime CreatedDate { get; set; }
+  }
+}
