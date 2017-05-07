ba81fbf3c0acc10d69b786e667a0ffafc88cb106
Lee-Roy Ashworth <leeroya@gmail.com>
2017-05-07 20:21:34 +0200

    feat: fileupload

--- tzatziki.minutz/Controllers/MeetingController.cs
+++ tzatziki.minutz/Controllers/MeetingController.cs
@@ -7,19 +7,27 @@ using tzatziki.minutz.models;
 using tzatziki.minutz.core;
 using tzatziki.minutz.models.Entities;
 using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
 
 namespace tzatziki.minutz.Controllers
 {
   [Authorize]
   public class MeetingController : BaseController
   {
+    private IHostingEnvironment _environment;
+
     public MeetingController(
       ITokenStringHelper tokenStringHelper,
       IProfileService profileService,
       IInstanceService instanceService,
       IOptions<AppSettings> settings,
-      IUserService userService) : base(settings, profileService, tokenStringHelper, instanceService, userService)
+      IUserService userService, IHostingEnvironment environment) : base(settings, profileService, tokenStringHelper, instanceService, userService)
     {
+      _environment = environment;
     }
 
     public IActionResult Index()
@@ -37,6 +45,25 @@ namespace tzatziki.minutz.Controllers
         meeting.Id = GetId();
 
       return Json(meeting);
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> FileUpload()
+    {
+      var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+      var uploadedFiles = new List<string>();
+      foreach (var file in Request.Form.Files)
+      {
+        if (file.Length > 0)
+        {
+          using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+          {
+            await file.CopyToAsync(fileStream);
+            uploadedFiles.Add(file.FileName);
+          }
+        }
+      }
+      return Json(uploadedFiles);
     }
 
     private string GetName()
