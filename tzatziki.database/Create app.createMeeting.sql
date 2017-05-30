fe292434da42ecf876e63ea0c6b91c23b1397ef7
Lee-Roy Ashworth <leeroya@gmail.com>
2017-05-30 05:14:07 +0200

    feat: MIN-4, Database-save-meeting-create

---
+++ tzatziki.database/Create app.createMeeting.sql
@@ -0,0 +1,54 @@
+﻿
+
+CREATE TABLE [Meeting] (
+        [Id] uniqueidentifier NOT NULL,
+        [Name] VARCHAR (MAX) NOT NULL,
+        [Location] VARCHAR (255) NULL,
+        [Date] DATETIME2 NULL,
+        [Time] VARCHAR (255) NULL,
+        [Duration] INT  NULL,
+        [IsReacurance] BIT  NULL,
+        [IsPrivate] BIT  NULL,
+        [ReacuranceType] VARCHAR (255) NULL,
+        [IsLocked] BIT  NULL,
+        [IsFormal] BIT  NULL,
+        [TimeZone] VARCHAR (255) NULL,
+        [Tag] VARCHAR (255) NULL,
+        [Purpose] VARCHAR (255) NULL,
+        [Outcome] VARCHAR (255) NULL
+)
+GO;
+
+CREATE TABLE [MeetingAttendee] (
+  [Id] uniqueidentifier NOT NULL,
+  [ReferanceId] uniqueidentifier NOT NULL,
+  [PersonIdentity]
+  [Role]
+
+)
+GO;
+
+CREATE TABLE [MeetingAgenda] (
+  [Id] uniqueidentifier NOT NULL,
+  [MeetingId] uniqueidentifier NOT NULL,
+)
+GO;
+
+CREATE TABLE [MeetingAction] (
+  [Id] uniqueidentifier NOT NULL,
+  [MeetingId] uniqueidentifier NOT NULL,
+)
+GO;
+
+CREATE TABLE [MeetingNote] (
+  [Id] uniqueidentifier NOT NULL,
+  [MeetingId] uniqueidentifier NOT NULL,
+)
+GO;
+
+CREATE TABLE [MeetingAttachment] (
+  [Id] uniqueidentifier NOT NULL,
+  [ReferanceId] uniqueidentifier NOT NULL,
+)
+GO;
+   
