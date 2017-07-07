52772e4a302dc018e6d1a91b00739e10dbd74ef4
Lee-Roy Ashworth <leeroya@gmail.com>
2017-07-07 21:39:15 +0200

    feat: implement gmail settings for testing

--- tzatziki.minutz.core/NotificationService.cs
+++ tzatziki.minutz.core/NotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using MailKit.Net.Smtp;
+using MimeKit;
 using tzatziki.minutz.interfaces;
 
 namespace tzatziki.minutz.core
@@ -13,10 +14,23 @@ namespace tzatziki.minutz.core
 
 		public bool InvitePerson()
 		{
+			var bodyBuilder = new BodyBuilder();
+			bodyBuilder.HtmlBody = @"<b>This is bold and this is <i>italic</i></b>";
+			//message.Body = bodyBuilder.ToMessageBody();
+
+			var message = new MimeMessage();
+			message.From.Add(new MailboxAddress("Minutz", "leeroya@gmail.com"));
+			message.To.Add(new MailboxAddress("Lee-Roy Ashworth", "leeroya@gmail.com"));
+			message.Subject = "Hello World - A mail from ASPNET Core";
+			message.Body = new TextPart("plain")
+			{
+				Text = "Hello World - A mail from ASPNET Core"
+			};
 			using (var client = new SmtpClient())
 			{
-				client.Connect("smtp.example.com", 587, false);
-				client.AuthenticationMechanisms.Remove("XOAUTH2");
+				client.Connect("smtp.gmail.com", 587, false);
+				client.Authenticate("leeroya@gmail.com", "7qwWi8gIzrI6");
+				//client.AuthenticationMechanisms.Remove("XOAUTH2");
 				// Note: since we don't have an OAuth2 token, disable 	// the XOAUTH2 authentication mechanism.     client.Authenticate("anuraj.p@example.com", "password");
 				client.Send(message);
 				client.Disconnect(true);
