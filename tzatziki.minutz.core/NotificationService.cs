f4dba8ad089746ae83ee2461c6f327dd597ff5d4
Lee-Roy Ashworth <leeroya@gmail.com>
2017-07-07 21:27:47 +0200

    feat: added Mailkit to handle the smtp for notifications

--- tzatziki.minutz.core/NotificationService.cs
+++ tzatziki.minutz.core/NotificationService.cs
@@ -1,4 +1,5 @@
-﻿using tzatziki.minutz.interfaces;
+﻿using MailKit.Net.Smtp;
+using tzatziki.minutz.interfaces;
 
 namespace tzatziki.minutz.core
 {
@@ -12,6 +13,14 @@ namespace tzatziki.minutz.core
 
 		public bool InvitePerson()
 		{
+			using (var client = new SmtpClient())
+			{
+				client.Connect("smtp.example.com", 587, false);
+				client.AuthenticationMechanisms.Remove("XOAUTH2");
+				// Note: since we don't have an OAuth2 token, disable 	// the XOAUTH2 authentication mechanism.     client.Authenticate("anuraj.p@example.com", "password");
+				client.Send(message);
+				client.Disconnect(true);
+			}
 			return true;
 		}
 	}
