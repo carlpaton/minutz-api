42414b6c518ace8db2c5d79da14c5f32b00f5ea4
Lee-Roy Ashworth <leeroya@gmail.com>
2017-09-01 21:21:14 +0200

    feat: prod url

--- src/minutz.ui/tools/env/prod.ts
+++ src/minutz.ui/tools/env/prod.ts
@@ -6,7 +6,7 @@ const ProdConfig: EnvConfig = {
   externalLoginPageUrl: 'https://opsoauth.mgsops.net:10251/#/grant?client_id=internal-dev&' +
   'redirect_uri=http%3A%2F%2Flocalhost%3A5555%2F%23%2Ftoken&state=45s9KYgjfj8gD0A2YvfNLw&response_type=code',
   tokenApiEndpointUrl: 'https://opsoauth.mgsops.net:10250/OAuth/Token',
-  tokenApiRedirectUrl: 'http://minutz-ui-48a627c6-1.3496b7f3.cont.dockerapp.io:5555/#/token',
+  tokenApiRedirectUrl: 'http://minutz-ui-48a627c6-1.3496b7f3.cont.dockerapp.io/#/token',
   client_id: 'internal-dev',
   client_secret: 'apipassword123'
 };
