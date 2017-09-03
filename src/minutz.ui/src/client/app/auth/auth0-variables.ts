575d0f4bba05671a2ba93857014f87976bb133c6
Lee-Roy Ashworth <leeroya@gmail.com>
2017-09-03 21:16:33 +0200

    chore: http://dockercloud-81f01559.cloudapp.net as the callback

--- src/minutz.ui/src/client/app/auth/auth0-variables.ts
+++ src/minutz.ui/src/client/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ export const AUTH_CONFIG: AuthConfig = {
   clientID: 'spEQucIOEbSBvkixqRHzkKDHNYRaGy3J',
   domain: 'tzatziki-minutz.auth0.com',
   //callbackURL: `${window.location}`
-  callbackURL: 'http://dockercloud-81f01559.cloudapp.net/'
+  callbackURL: 'http://dockercloud-81f01559.cloudapp.net'
 };
