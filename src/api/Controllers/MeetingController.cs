87ec4bdcafb0576ab8148b62b6bcdf836c7766fd
Lee-Roy Ashworth <leeroya@gmail.com>
2017-09-09 09:03:15 +0200

    feat: moved the api folder to in the src folder - moved settings to environment variables

---
+++ src/api/Controllers/MeetingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class MeetingController : Controller
+    {
+        [HttpGet]
+        public minutz.models.ViewModels.Meeting Get()
+        {
+            return new minutz.models.ViewModels.Meeting();
+        }
+    }
+}
---
+++ src/minutz-models/ViewModels/Meeting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minutz.models.ViewModels
+{
+    public class Meeting
+    {
+    }
+}
