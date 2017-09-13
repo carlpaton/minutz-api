088b372212152a2a11bb6b9acfde0ab2e1f5a97c
Lee-Roy Ashworth <leeroya@gmail.com>
2017-09-13 22:47:32 +0200

    feat: Add MeetingAction

---
+++ src/Models/Entities/MeetingAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Models.Entities
+{
+  public class MeetingAction
+  {
+    public Guid Id { get; set; }
+    public Guid ReferanceId { get; set; }
+    public string ActionText { get; set; }
+    public Guid PersonId { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsComplete { get; set; }
+  }
+}
---
+++ src/SqlRepository/MeetingActionRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Models.Entities;
+using System.Data;
+using System.Linq;
+using System;
+using Dapper;
+
+namespace SqlRepository
+{
+  public class MeetingActionRepository
+  {
+    public MeetingAction Get(Guid id, string schema, string connectionString)
+    {
+      if (id == Guid.NewGuid() || string.IsNullOrEmpty(schema) || string.IsNullOrEmpty(connectionString))
+        throw new ArgumentException("Please provide a valid meeting identifier, schema or connectionstring.");
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var sql = $"select * from [{schema}].[MeetingAction] WHERE Id = '{id.ToString()}'";
+        var data = dbConnection.Query<MeetingAction>(sql).FirstOrDefault();
+        return data;
+      }
+    }
+    public IEnumerable<MeetingAction> List(string schema, string connectionString)
+    {
+      if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(schema))
+        throw new ArgumentException("Please provide a valid schema or connectionstring.");
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var sql = $"select * from [{schema}].[MeetingAction]";
+        var data = dbConnection.Query<MeetingAction>(sql).ToList();
+        return data;
+      }
+    }
+
+    public bool Add(MeetingAction action,string schema, string connectionString)
+    {
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var insertSql = $@"insert into [{schema}].[MeetingAction](
+                                                                 [Id]
+                                                                ,[ReferanceId]
+                                                                ,[ActionText]
+                                                                ,[PersonId]
+                                                                ,[DueDate]
+                                                                ,[IsComplete]
+                                                                ) 
+                                                         values(
+                                                                 @Id
+                                                                ,@ReferanceId
+                                                                ,@ActionText
+                                                                ,@PersonId
+                                                                ,@DueDate
+                                                                ,@IsComplete)";
+        var instance = dbConnection.Execute(insertSql, new
+                                                        {
+                                                          action.Id,
+                                                          action.ReferanceId,
+                                                          action.ActionText,
+                                                          action.PersonId,
+                                                          action.DueDate,
+                                                          action.IsComplete
+                                                        });
+        return instance == 1;
+      }
+    }
+
+      
+  }
+}
