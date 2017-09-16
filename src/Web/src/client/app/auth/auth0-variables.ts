2d3903753cdba20baf621164429b266930a931e6
Lee-Roy Ashworth <leeroya@gmail.com>
2017-09-16 06:49:03 +0200

    building for test

--- src/Web/src/client/app/auth/auth0-variables.ts
+++ src/Web/src/client/app/auth/auth0-variables.ts
@@ -8,6 +8,6 @@ export const AUTH_CONFIG: AuthConfig = {
     clientID: 'jqdE5upkdDHejU2wJpzHRXYBEwFtsH3N',
     domain: 'dockerdurban.auth0.com',
     audience: 'https://minutz.net',
-    callbackURL: 'http://localhost:4200/',
-    //callbackURL: 'http:stage-minutz.net'
+    //callbackURL: 'http://localhost:4200/',
+    callbackURL: 'http://test-api.minutz.net'
 };
