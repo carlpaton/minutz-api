5af6e537c39efa73fec9d7ea571996ed987d1965
Lee-Roy Ashworth <leeroya@gmail.com>
2017-09-18 21:00:40 +0200

    bug: adding agenda items
    - quick add and normal add functioning but passing the collection through.

--- src/Web/src/client/app/meeting/agendaPanel/agenda-panel.component.ts
+++ src/Web/src/client/app/meeting/agendaPanel/agenda-panel.component.ts
@@ -18,56 +18,60 @@ declare let $: any;
     styleUrls: ['agenda-panel.component.css']
 })
 export class AgendaPanelComponent implements OnInit, OnChanges {
-    Name:string;
+    Name: string;
     QuickTopic: string;
     @Input() Id: string;
-    @Input() AgendaCollection : Array<MeetingAgenda>;
+    @Input() AgendaCollection: Array<MeetingAgenda>;
     @Output() Click = new EventEmitter();
     @Output() Topic = new EventEmitter<MeetingAgenda>();
+    @Output() CollectionNotification = new EventEmitter<Array<MeetingAgenda>>();
     private createId(): any {
         return `${this.createidsection()}-${this.createidsection()}-${this.createidsection()}-${this.createidsection()}` +
-               `${this.createidsection()}-${this.createidsection()}-${this.createidsection()}-${this.createidsection()}`;
+            `${this.createidsection()}-${this.createidsection()}-${this.createidsection()}-${this.createidsection()}`;
     }
     private createidsection() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
-    public ngOnChanges(changes : SimpleChanges) {
+    public ngOnChanges(changes: SimpleChanges) {
         console.log(changes);
     }
     public ngOnInit() {
         if (!this.Id) {
             this.Name = this.createId();
-        }else {
+        } else {
             this.Name = this.Id;
         }
-        if(!this.AgendaCollection) {
+        if (!this.AgendaCollection) {
             this.AgendaCollection = [];
         }
     }
-    public AddTopic($event : any) {
-        if(!this.AgendaCollection) {
+    public AddTopic($event: any) {
+        if (!this.AgendaCollection) {
             this.AgendaCollection = [];
         }
         let quickTopic = new MeetingAgenda();
         quickTopic.AgendaHeading = this.QuickTopic;
-        quickTopic.DurationLabel = '00:00';
+        quickTopic.Duration = 100;
+        quickTopic.DurationLabel = '1 minute';
+        quickTopic.AgendaText = 'Quick item .......';
         this.AgendaCollection.push(quickTopic);
+        this.CollectionNotification.emit(this.AgendaCollection);
     }
-    public click():void {
+    public click(): void {
         this.Click.emit();
     }
-    public allowDrop(ev : any) {
+    public allowDrop(ev: any) {
         ev.preventDefault();
     }
-    public drag(ev : any) {
+    public drag(ev: any) {
         ev.dataTransfer.setData("text", ev.target.id);
     }
-    public drop(ev : any) {
+    public drop(ev: any) {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         ev.target.appendChild(document.getElementById(data));
     }
-    public SelectTopic(agenda : MeetingAgenda) {
+    public SelectTopic(agenda: MeetingAgenda) {
         this.Topic.emit(agenda);
     }
 }
