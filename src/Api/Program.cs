c84554c8b302cd0a375df0cca9a01e9d542ce559
Lee-Roy <Lee-Roy.Ashworth@derivco.co.za>
2017-11-13 22:44:16 +0200

    feat: general build and hosting

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Api
 {
