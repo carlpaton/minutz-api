331165a5ceaade4e53c78b7e2712e05babab82d4
Lee-Roy <Lee-Roy.Ashworth@derivco.co.za>
2017-11-15 08:33:48 +0200

    chore: setup vscode debugging for transition to Mac PC

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,5 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 
 namespace Api
 {
