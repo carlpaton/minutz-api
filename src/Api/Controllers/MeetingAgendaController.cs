e6383ce8ed19a08fb09d0cc8bb8e8c8560a24c95
Lee-Roy <Lee-Roy.Ashworth@derivco.co.za>
2017-11-23 20:44:57 +0200

    chore: house keeping : MeetingAgenda Controller

--- src/Api/Controllers/MeetingAgendaController.cs
+++ src/Api/Controllers/MeetingAgendaController.cs
@@ -7,7 +7,6 @@ using Models.Entities;
 
 namespace Api.Controllers
 {
-  //[Route ("api/[controller]")]
   public class MeetingAgendaController : Controller
   {
     private readonly IMeetingService _meetingService;
@@ -20,12 +19,12 @@ namespace Api.Controllers
     /// Get all agenda items for a meeting
     /// </summary>
     /// <returns>Collection of MeetingAgenda objects</returns>
-    [HttpGet("api/meeting/{meetingId}/agenda")]
+    [HttpGet ("api/meeting/{meetingId}/agenda")]
     [Authorize]
     public List<MeetingAgenda> Get ([FromRoute] string meetingId)
     {
       var token = Request.Headers.FirstOrDefault (i => i.Key == "Authorization").Value;
-      return new List<MeetingAgenda>();
+      return new List<MeetingAgenda> ();
     }
 
     /// <summary>
@@ -44,31 +43,31 @@ namespace Api.Controllers
     /// Create a meeting Agenda
     /// </summary>
     /// <returns></returns>
-    [HttpPut("api/meeting/{ReferenceId}/agenda")]
+    [HttpPut ("api/meeting/{ReferenceId}/agenda")]
     [Authorize]
     public MeetingAgenda Put (MeetingAgenda agenda)
     {
       var token = Request.Headers.FirstOrDefault (i => i.Key == "Authorization").Value;
-      return new MeetingAgenda();
+      return new MeetingAgenda ();
     }
 
     /// <summary>
     /// Update the Meeting Agenda
     /// </summary>
     /// <returns></returns>
-    [HttpPost("api/meeting/{ReferenceId}/agenda/{id}")]
+    [HttpPost ("api/meeting/{ReferenceId}/agenda/{id}")]
     [Authorize]
     public MeetingAgenda Post (MeetingAgenda agenda)
     {
       var token = Request.Headers.FirstOrDefault (i => i.Key == "Authorization").Value;
-      return new MeetingAgenda();
+      return new MeetingAgenda ();
     }
 
     /// <summary>
     /// Delete the single instance of the agenda item.
     /// </summary>
     /// <returns>true if successful and false if failure.</returns>
-    [HttpDelete("api/meeting/{meetingId}/agenda/{id}")]
+    [HttpDelete ("api/meeting/{meetingId}/agenda/{id}")]
     [Authorize]
     public bool Delete ([FromRoute] string meetingId, string id)
     {
