fd420f20c0f8de58ec579631c3a114d5726a0a02
Lee-Roy <Lee-Roy.Ashworth@derivco.co.za>
2017-12-01 23:32:20 +0200

    feat: attachment repo update

--- src/SqlRepository/MeetingAttachmentRepository.cs
+++ src/SqlRepository/MeetingAttachmentRepository.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Interface.Repositories;
+using System.Data.SqlClient;
 using Models.Entities;
+using System.Data;
+using System.Linq;
+using Dapper;
+using System;
 
 namespace SqlRepository
 {
@@ -9,27 +13,105 @@ namespace SqlRepository
   {
     public MeetingAttachment Get(Guid id, string schema, string connectionString)
     {
-      throw new NotImplementedException();
+      if (id == Guid.NewGuid() || string.IsNullOrEmpty(schema) || string.IsNullOrEmpty(connectionString))
+        throw new ArgumentException("Please provide a valid meeting agenda identifier, schema or connection string.");
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var sql = $"select * from [{schema}].[MeetingAttachment] WHERE Id = '{id.ToString()}'";
+        var data = dbConnection.Query<MeetingAttachment>(sql).FirstOrDefault();
+        return data;
+      }
     }
     public List<MeetingAttachment> GetMeetingAttachments(Guid referenceId, string schema, string connectionString)
     {
-      throw new NotImplementedException();
+      if (referenceId == Guid.NewGuid() || string.IsNullOrEmpty(schema) || string.IsNullOrEmpty(connectionString))
+        throw new ArgumentException("Please provide a valid meeting agenda identifier, schema or connection string.");
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var sql = $"select * from [{schema}].[MeetingAttachment] WHERE ReferenceId = '{referenceId.ToString()}'";
+        var data = dbConnection.Query<MeetingAttachment>(sql);
+        return data.ToList();
+      }
     }
     public IEnumerable<MeetingAttachment> List(string schema, string connectionString)
     {
-      throw new NotImplementedException();
+      if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(schema))
+        throw new ArgumentException("Please provide a valid schema or connection string.");
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var sql = $"select * from [{schema}].[MeetingAttachment]";
+        var data = dbConnection.Query<MeetingAttachment>(sql).ToList();
+        return data;
+      }
     }
-    public bool Add(MeetingAttachment action, string schema, string connectionString)
+    public bool Add(MeetingAttachment attachment, string schema, string connectionString)
     {
-      throw new NotImplementedException();
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        string insertSql = $@"insert into [{schema}].[MeetingAttachment](
+                                                                 [Id]
+                                                                ,[ReferenceId]
+                                                                ,[FileName]
+                                                                ,[MeetingAttendeeId]
+                                                                ,[Date]
+                                                                ,[FileData]
+                                                                ) 
+                                                         values(
+                                                                 @Id
+                                                                ,@ReferenceId
+                                                                ,@FileName
+                                                                ,@MeetingAttendeeId
+                                                                ,@Date
+                                                                ,@FileData
+                                                                )";
+        var instance = dbConnection.Execute(insertSql, new
+        {
+          attachment.Id,
+          attachment.ReferanceId,
+          attachment.FileName,
+          attachment.MeetingAttendeeId,
+          attachment.Date,
+          attachment.FileData
+        });
+        return instance == 1;
+      }
     }
-    public bool Update(MeetingAttachment action, string schema, string connectionString)
+    public bool Update(MeetingAttachment attachment, string schema, string connectionString)
     {
-      throw new NotImplementedException();
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        string updateQuery = $@"UPDATE [{schema}].[MeetingAttachment] 
+                             SET ReferenceId = @ReferenceId, 
+                                 FileName = @FileName, 
+                                 MeetingAttendeeId = @MeetingAttendeeId, 
+                                 Date = @Date,
+                                 FileData = @FileData
+                             WHERE Id = @Id";
+        var instance = dbConnection.Execute(updateQuery, new
+        {
+          attachment.ReferanceId,
+          attachment.FileName,
+          attachment.MeetingAttendeeId,
+          attachment.Date,
+          attachment.FileData
+        });
+        return instance == 1;
+      }
     }
     public bool Delete(Guid attachmentId, string schema, string connectionString)
     {
-      throw new NotImplementedException();
+      using (IDbConnection dbConnection = new SqlConnection(connectionString))
+      {
+        dbConnection.Open();
+        var sql = $"DELETE from [{schema}].[MeetingAttachment] WHERE Id = '{attachmentId.ToString()}'";
+        var instance = dbConnection.Execute(sql);
+        return instance == 1;
+      }
     }
   }
 }
