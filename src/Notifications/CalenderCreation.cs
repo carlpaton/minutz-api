ab8058ace3d40df738fa3f7c81430eca9af5c1e5
info@docker.durban <info@docker.durban>
2018-01-02 12:05:13 +0200

    feat: working with the ical attachment

---
+++ src/Notifications/CalenderCreation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+
+namespace Notifications
+{
+	public class CalenderCreation
+	{
+		public void Reacurring()
+		{
+			var now = DateTime.Now;
+			var later = now.AddHours(1);
+
+			//Repeat daily for 5 days
+			var rrule = new RecurrencePattern(FrequencyType.Daily, 1) { Count = 5 };
+
+			var e = new CalendarEvent
+			{
+				Start = new CalDateTime(now),
+				End = new CalDateTime(later),
+				RecurrenceRules = new List<RecurrencePattern> { rrule },
+			};
+
+			var calendar = new Calendar();
+			calendar.Events.Add(e);
+
+			var serializer = new CalendarSerializer();
+			var serializedCalendar = serializer.SerializeToString(calendar);
+		}
+	}
+}
