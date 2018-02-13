133613884f14c2b907587ff80e71e08ed46eb49b
Lee-Roy Ashworth <leeroya@gmail.com>
2018-02-13 12:37:22 +0200

    feat: added logging - logging - auth header validation to help against spamming

--- src/Api/Controllers/UserController.cs
+++ src/Api/Controllers/UserController.cs
@@ -11,29 +11,51 @@ namespace Api.Controllers
 {
   public class UserController : Controller
   {
+    internal string _auth = "auth";
     private readonly IUserValidationService _userValidationService;
     private readonly IAuthenticationService _authenticationService;
     private readonly IValidationService _validationService;
+
+    private readonly ILogService _logService;
     public UserController (
       IUserValidationService userValidationService,
       IAuthenticationService authenticationService,
-      IValidationService validationService)
+      IValidationService validationService,
+      ILogService logService)
     {
       this._userValidationService = userValidationService;
       this._authenticationService = authenticationService;
       this._validationService = validationService;
+      this._logService = logService;
     }
 
     [HttpPost("api/user/login", Name = "Log in the user.")]
     public IActionResult Login (string username, string password)
     {
-      //var emailValidation = this._validationService.ValidEmail(email);
-      //var passwordValidation = this._validationService.ValidPassword(password);
-      if(string.IsNullOrEmpty(username))
+      var hasHeader = Request.Headers.ToList().Any(i => i.Key == "x-auth-header");
+      if(!hasHeader)
+      {
+        this._logService.Log(Minutz.Models.LogLevel.Info, "The request did not have x-auth-header header.");
         return StatusCode(404,"please provide a valid username or password");
+      }
+      var authHeaderValue = Request.Headers.ToList().First(i => i.Key == "x-auth-header").Value;
+      if(authHeaderValue != this._auth)
+      {
+        this._logService.Log(Minutz.Models.LogLevel.Info, "The request had a x-auth-header header, but the value did not match the configuration for the instance.");
+        return StatusCode(404,"please provide a valid username or password");
+      }
 
-      if(string.IsNullOrEmpty(password))
+      if(string.IsNullOrEmpty(username))
+      {
+        this._logService.Log(Minutz.Models.LogLevel.Info, "The request did not have a username.");
         return StatusCode(404,"please provide a valid username or password");
+      }
+      
+      if(string.IsNullOrEmpty(password))
+      {
+        this._logService.Log(Minutz.Models.LogLevel.Info, "The request did not have a password.");
+        return StatusCode(404,"please provide a valid username or password");
+      }
 
       var loginResult = this._authenticationService.Login(username,password);
       if(loginResult.condition)
