1a3436a5698f8ab1a3381a39625e71576b6c4da3
Lee-Roy Ashworth <leeroya@gmail.com>
2018-03-19 22:20:36 +0200

    feat: updated Report service

--- src/Core/JsReportService.cs
+++ src/Core/JsReportService.cs
@@ -1,5 +1,8 @@
-﻿using Interface.Repositories;
+﻿using System;
+using System.Collections.Generic;
+using Interface.Repositories;
 using Interface.Services;
+using Minutz.Models.Entities;
 
 namespace Core
 {
@@ -10,6 +13,43 @@ namespace Core
     public JsReportService(IReportRepository reportRepository)
     {
       _reportRepository = reportRepository;
+    }
+
+
+    public (bool condition, string message, byte[] file) CreateMinutes
+      (Meeting meetingItem, IEnumerable<MeetingAttendee> attendeescollection, IEnumerable<MeetingAgenda> agendaCollection, IEnumerable<MeetingNote> notecollection)
+    {
+      var agenditems = new List<dynamic>();
+      var noteItems = new List<dynamic>();
+      var people = new List<dynamic>();
+      foreach (var agenda in agendaCollection)
+      {
+        agenditems.Add(new {agendaHeading = agenda.AgendaHeading ,agendaText = agenda.AgendaText });
+      }
+        
+      foreach (var note in notecollection)
+      {
+        noteItems.Add(new {agendaHeading = note.NoteText });
+      } 
+      
+      foreach (var attendee in attendeescollection)
+      {
+        noteItems.Add(new {name = attendee.Name, role = attendee.Role });
+      } 
+         
+      var meeting = new
+      {
+        name = meetingItem.Name,
+        date = meetingItem.Date,
+        location = meetingItem.Location,
+        time = meetingItem.Time,
+        purpose = meetingItem.Purpose,
+        outcome = meetingItem.Outcome,
+        agenda = agenditems,
+        attendees = people,
+        notes = noteItems
+      };
+      return _reportRepository.CreateMinutesReport(meeting);
     }
   }
 }
