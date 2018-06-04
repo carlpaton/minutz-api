fbc81e2a9d3ed595f905f37e0d50c690c849b59c
Lee-Roy Ashworth <leeroya@gmail.com>
2018-06-04 21:31:56 +0200

    MIN-177 feat: created AdminConmtroller.cs feat: CRUD for user feat: Get CAL

---
+++ src/Api/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Api.Extensions;
+using Api.Models;
+using Interface;
+using Interface.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Minutz.Models.Entities;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Api.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize]
+        [HttpGet("api/admin/users")]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+        
+        [Authorize]
+        [HttpPut("api/admin/user")]
+        public IActionResult CreateUser()
+        {
+            return Ok();
+        }
+        
+        [Authorize]
+        [HttpPost("api/admin/user")]
+        public IActionResult UpdateUser()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpDelete("api/admin/user")]
+        public IActionResult DeleteUser()
+        {
+            return Ok();
+        }
+        
+        [Authorize]
+        [HttpGet("api/admin/cal")]
+        public IActionResult GetCal()
+        {
+            return Ok();
+        }
+        
+    }
+}
